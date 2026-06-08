cf812cfe1aec8872194b0595df76d0284325788d
maxirattero <maxirattero87@gmail.com>
2026-06-08 09:30:26 -0300

    creacion de Entidades Simples y sus propiedades

---
+++ Panaderia.Models/Entities/CategoriaProducto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Panaderia.Models.Entities
+{
+    public class CategoriaProducto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre de la categoría es obligatorio.")]
+        public string Nombre { get; set; } = string.Empty;
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+    }
+}
---
+++ Panaderia.Models/Entities/Formato.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Panaderia.Models.Entities
+{
+    public class Formato
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio.")]
+        public string Descripcion { get; set; } = string.Empty;
+    }
+}
