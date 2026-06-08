af87dca690d54ac5f5ab2b106e99a7339afab064
maxirattero <maxirattero87@gmail.com>
2026-06-08 13:34:06 -0300

    Todas las entidades creadas

---
+++ Panaderia.Models/Entities/ReporteCaja.cs
@@ -0,0 +1,18 @@
+﻿using Panaderia.Models.Enums;
+
+namespace Panaderia.Models.Entities
+{
+    public class ReporteCaja
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public TipoMovimiento Tipo { get; set; } = TipoMovimiento.Ingreso;
+        public CategoriaMovimiento Categoria { get; set; } = CategoriaMovimiento.Otro;
+        public decimal Monto { get; set; }
+        public string? Descripcion { get; set; }
+        public int? IdPedido { get; set; }
+        public Pedido? Pedido { get; set; }
+        public int? IdProveedor { get; set; }
+        public Proveedor? Proveedor { get; set; }
+    }
+}
