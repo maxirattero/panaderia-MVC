e93266b8edb52c6457d6b55621472ea763a91bea
maxirattero <maxirattero87@gmail.com>
2026-06-08 09:30:26 -0300

    creacion de Entidades Simples y sus propiedades

---
+++ Panaderia.Models/Entities/Proveedor.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Panaderia.Models.Entities
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio.")]
+        public string Nombre { get; set; } = string.Empty;
+        public string? Contacto { get; set; }
+        public string? Telefono { get; set; }
+        public string? Email { get; set; }
+        public string? Direccion { get; set; }
+        public string? Notas { get; set; }
+        public bool Activo { get; set; } = true;
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+    }
+}
