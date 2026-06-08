a7954ee3e69566fc5870e2e83fe9fd04ee1e7ac6
maxirattero <maxirattero87@gmail.com>
2026-06-08 13:34:06 -0300

    Todas las entidades creadas

---
+++ Panaderia.Models/Entities/DetallePedido.cs
@@ -0,0 +1,13 @@
+﻿namespace Panaderia.Models.Entities
+{
+    public class DetallePedido
+    {
+        public int Id { get; set; }
+        public int IdPedido { get; set; }
+        public int IdProductoFinal { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public Pedido Pedido { get; set; } = null!;
+        public ProductoFinal ProductoFinal { get; set; } = null!;
+    }
+}
---
+++ Panaderia.Models/Entities/Pedido.cs
@@ -0,0 +1,21 @@
+﻿using Panaderia.Models.Enums;
+
+namespace Panaderia.Models.Entities
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public EstadoPedido Estado { get; set; } = EstadoPedido.Pendiente;
+        public DateTime? FechaEntrega { get; set; }
+        public decimal MontoTotal { get; set; }
+        public string? Notas { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+        public decimal MontoCobrado { get; set; } = 0;
+        public ICollection<DetallePedido> Detalles { get; set; } = new List<DetallePedido>();
+        public ICollection<ReporteCaja> Reportes { get; set; } = new List<ReporteCaja>();
+        public Cliente Cliente { get; set; } = null!;
+
+    }
+}
