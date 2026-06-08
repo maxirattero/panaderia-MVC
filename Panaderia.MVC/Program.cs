d9615e6fb12559c5220a4966a6f59af68739e8f6
maxirattero <maxirattero87@gmail.com>
2026-06-08 14:22:37 -0300

    DbContext y migracion inicial

--- Panaderia.MVC/Program.cs
+++ Panaderia.MVC/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Panaderia.Models.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<PanaderiaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
