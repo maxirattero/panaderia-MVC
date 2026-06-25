a98621a450eae4da851e56818e1cb4690ff9870f
maxirattero <maxirattero87@gmail.com>
2026-06-25 12:44:53 -0300

    Configurar deplot: DATABASE_URL, migraciones automaticas, User Secrets

--- Panaderia.MVC/Program.cs
+++ Panaderia.MVC/Program.cs
@@ -10,8 +10,31 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+string? connectionString;
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':', 2);
+    var npgsqlBuilder = new Npgsql.NpgsqlConnectionStringBuilder
+    {
+        Host                   = uri.Host,
+        Port                   = uri.Port,
+        Database               = uri.AbsolutePath.TrimStart('/'),
+        Username               = userInfo[0],
+        Password               = userInfo.Length > 1 ? Uri.UnescapeDataString(userInfo[1]) : "",
+        SslMode                = Npgsql.SslMode.Require,
+        TrustServerCertificate = true
+    };
+    connectionString = npgsqlBuilder.ConnectionString;
+}
+else
+{
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+
 builder.Services.AddDbContext<PanaderiaContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddScoped<IProveedorService, ProveedorService>();
 builder.Services.AddScoped<IClienteService, ClienteService>();
@@ -30,6 +53,21 @@ builder.Services.AddDataProtection()
     .SetApplicationName("MasaViva");
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PanaderiaContext>();
+    try
+    {
+        db.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Database migration failed on startup");
+        throw;
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
